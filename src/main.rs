7398cf673f956da7e4eceefa42d25988f7ea49fe
Ross Baquir <streakinthesky@gmail.com>
2024-05-31 12:31:43 -0700

    commands to match

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,66 @@
-#[allow(unused_imports)]
 use std::env;
-#[allow(unused_imports)]
+use std::error;
 use std::fs;
+use std::fmt;
+use std::io;
+use flate2::read::GzDecoder;
 
-fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+#[derive(Debug, PartialEq, Eq)]
+enum Error {
+    UnknownCommand(String),
 
-    // Uncomment this block to pass the first stage
-    let args: Vec<String> = env::args().collect();
-    if args[1] == "init" {
-        fs::create_dir(".git").unwrap();
-        fs::create_dir(".git/objects").unwrap();
-        fs::create_dir(".git/refs").unwrap();
-        fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
-        println!("Initialized git directory")
-    } else {
-        println!("unknown command: {}", args[1])
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match &self {
+            Error::UnknownCommand(command) => write!(f, "unknown command: {command}"),
+        }
     }
 }
+
+impl error::Error for Error {}
+
+fn init() -> Result<String, io::Error> {
+    fs::create_dir(".git")?;
+    fs::create_dir(".git/objects")?;
+    fs::create_dir(".git/refs")?;
+    fs::write(".git/HEAD", "ref: refs/heads/main\n")?;
+    Ok("Initialized git directory".to_string())
+}
+
+fn unknown_command(command: &str) -> Result<String, Error> {
+    Err(Error::UnknownCommand(command.to_string()))
+}
+
+    let args: Vec<String> = env::args().collect();
+    let response: Result<String, Box<dyn error::Error>>  = match args[1].as_str() {
+        "init" => init().map_err(|err|Box::new(err) as Box<dyn error::Error>),
+        _ => unknown_command(&args[1]).map_err(|err|Box::new(err) as Box<dyn error::Error>)
+    };
+
+    match response {
+        Ok(response) => {
+            println!("{response}");
+        },
+        Err(error) => {
+            println!("{error}");
+            std::process::exit(1);
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_unknown_command() {
+        let command = "unknown-command";
+        let expected_error = Err(Error::UnknownCommand(command.to_string()));
+
+        let actual_result = unknown_command(command);
+        assert_eq!(actual_result, expected_error);
+    }
+}
+
